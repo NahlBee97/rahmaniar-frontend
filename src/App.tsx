2aa4e153337b93f3c42f76e2df62501d166b0bf8
Nahalil <nahalilmuchtar2@gmail.com>
2025-11-25 10:43:22 +0800

    complete initial setup

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h2 className="text-red-500">Hello World</h2>
-}
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+import Home from "./pages/home";
+import NotFound from "./pages/notFound";
+import About from "./pages/about";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <NotFound/>
+  },
+  {
+    path: "/about",
+    element: <About/>
+  }
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
